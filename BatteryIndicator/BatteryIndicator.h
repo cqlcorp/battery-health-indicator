2ab3fb8586cf15f1637d303b51ece3fc92bf7906
Kevin Wolf <kevin.wolf@cqlcorp.com>
2017-07-03 11:38:07 -0400

    converting to cocoa pod

---
+++ BatteryIndicator/BatteryIndicator.h
@@ -0,0 +1,19 @@
+//
+//  BatteryIndicator.h
+//  BatteryIndicator
+//
+//  Created by Kevin Wolf on 7/3/17.
+//  Copyright © 2017 CQL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BatteryIndicator.
+FOUNDATION_EXPORT double BatteryIndicatorVersionNumber;
+
+//! Project version string for BatteryIndicator.
+FOUNDATION_EXPORT const unsigned char BatteryIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BatteryIndicator/PublicHeader.h>
+
+
